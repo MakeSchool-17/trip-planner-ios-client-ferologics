3dcbc3b46794dfe8ea25a3b871908ddc8e98c271
ferologics <f.hetes@gmail.com>
2015-10-22 23:23:03 -0700

    Create new project

---
+++ Tripologics_planner/Tripologics_planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tripologics_planner
+//
+//  Created by mbp_13 on 22/10/15.
+//  Copyright © 2015 ferologics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
